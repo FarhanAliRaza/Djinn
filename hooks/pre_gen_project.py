dfa1d923800e889bd7bb396041ba2fb9cad3dece
farhan <Www.mfarvirus@gmail.com>
2024-04-01 01:44:43 +0500

    cookiecutter confguration

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+module_name = "{{ cookiecutter.project_slug }}"
+
+
+if __name__ == "__main__":
+
+    if not re.match(MODULE_REGEX, module_name):
+        print(f"ERROR: {module_name} is not a valid Python module name!")
+        sys.exit(1)
--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,9 +1,5 @@
-import re
 import subprocess
 import sys
-
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
-module_name = "{{ cookiecutter.module_name }}"
 
 
 def is_docker_installed() -> bool:
@@ -17,8 +13,4 @@ def is_docker_installed() -> bool:
 if __name__ == "__main__":
     if not is_docker_installed():
         print("ERROR: Docker is not installed.")
-        sys.exit(1)
-
-    if not re.match(MODULE_REGEX, module_name):
-        print(f"ERROR: {module_name} is not a valid Python module name!")
         sys.exit(1)
